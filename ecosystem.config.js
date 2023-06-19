1115a9e9ef97dbf69b889ccdc00bf79216dfa10d
alesikivan <obcofficiall@gmail.com>
2023-06-19 10:43:28 +0300

    updated ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       repo : 'https://github.com/alesikivan/digital-city-server',
       path : '/root/apps/digital-city',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'npm install && pm2 delete maps && pm2 start maps.js && pm2 save --force',
       'pre-setup': ''
     }
   }
